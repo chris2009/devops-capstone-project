33453cb693962740f807843ae4157785b566709b
chris2009 <christian.1582@gmail.com>
2024-01-06 03:21:41 -0500

    add error handler in update account tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -176,6 +176,11 @@ class TestAccountService(TestCase):
         response = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
     
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    
     ####################################################
     # TEST DELETE AN ACCOUNT
     ####################################################
@@ -184,3 +189,4 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+    
